0fe1c164009c396b288190003a198ecc89dff716
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2023-09-04 15:50:17 +0530

    Add Support for Azure CNI Overlay

---
+++ Azure-CNI-Overlay/variables.tf
@@ -0,0 +1,18 @@
+# Plan Block Variables
+variable "plan_name" {
+  description = "Plan Name"
+  type        = string
+  default     = "isovalent-cilium-enterprise-base-edition"
+}
+
+variable "plan_product" {
+  description = "Plan Product"
+  type        = string
+  default     = "isovalent-cilium-enterprise"
+}
+
+variable "plan_publisher" {
+  description = "Plan Publisher"
+  type        = string
+  default     = "isovalentinc1662143158090"
+}
