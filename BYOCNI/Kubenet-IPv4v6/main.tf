b6106c15f1dc6479c105b4b2f890535e9492a082
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2023-09-04 15:44:09 +0530

    Add support for Dual Stack in Kubenet

---
+++ BYOCNI/Kubenet-IPv4v6/main.tf
@@ -0,0 +1,57 @@
+resource "azurerm_resource_group" "azurecilium" {
+  name     = "azurecilium"
+  location = "canadacentral"
+}
+
+resource "azurerm_virtual_network" "azurecilium" {
+  name                = "azurecilium-vnet"
+  address_space       = ["192.168.10.0/24"]
+  location            = azurerm_resource_group.azurecilium.location
+  resource_group_name = azurerm_resource_group.azurecilium.name
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  virtual_network_name = azurerm_virtual_network.azurecilium.name
+  address_prefixes     = ["192.168.10.0/24"]
+  resource_group_name  = azurerm_resource_group.azurecilium.name
+}
+
+resource "azurerm_virtual_network" "azurecilium6" {
+  name                = "azurecilium-vnet6"
+  address_space       = ["1900::/64"]
+  location            = azurerm_resource_group.azurecilium.location
+  resource_group_name = azurerm_resource_group.azurecilium.name
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
+  virtual_network_name = azurerm_virtual_network.azurecilium6.name
+  address_prefixes     = ["1900::/64"]
+  resource_group_name  = azurerm_resource_group.azurecilium.name
+}
+
+resource "azurerm_kubernetes_cluster" "azurecilium" {
+  name                = "azurecilium"
+  location            = azurerm_resource_group.azurecilium.location
+  resource_group_name = azurerm_resource_group.azurecilium.name
+  dns_prefix          = "azurecilium"
+  default_node_pool {
+    name       = "azurecilium"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "kubenet"
+    ip_versions    = ["IPv4", "IPv6"]
+    pod_cidr       = "10.10.0.0/22"
+    service_cidr   = "10.20.0.0/24"
+    dns_service_ip = "10.20.0.10"
+    pod_cidrs      = ["10.10.0.0/22", "2001::/64"]
+    service_cidrs  = ["10.20.0.0/24", "2001:1::/108"]
+  }
+}
