fdec954c52fdc1fe98fd15668562a95d2035f8c7
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2023-12-28 20:13:43 +0530

    Directory Structure Changes

---
+++ Azure-CNI-Netpol-Calico/main.tf
@@ -0,0 +1,38 @@
+resource "azurerm_resource_group" "azurecni" {
+  name     = "azurecnirg"
+  location = "canadacentral"
+}
+
+resource "azurerm_virtual_network" "azurecni" {
+  name                = "azurecnirg-vnet"
+  address_space       = ["10.0.0.0/8"]
+  location            = azurerm_resource_group.azurecni.location
+  resource_group_name = azurerm_resource_group.azurecni.name
+}
+
+resource "azurerm_subnet" "azurecni" {
+  name                 = "azurecnirg-subnet"
+  resource_group_name  = azurerm_resource_group.azurecni.name
+  virtual_network_name = azurerm_virtual_network.azurecni.name
+  address_prefixes     = ["10.10.0.0/16"]
+}
+
+resource "azurerm_kubernetes_cluster" "azurecni" {
+  name                = "azurecniaks"
+  location            = azurerm_resource_group.azurecni.location
+  resource_group_name = azurerm_resource_group.azurecni.name
+  dns_prefix          = "azurecniaks"
+  default_node_pool {
+    name           = "default"
+    node_count     = 2
+    vm_size        = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.azurecni.id
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "calico"
+  }
+}
