086b634d901c414a8ea8025d66e3d36851b978a5
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2023-12-28 23:37:47 +0530

    Construct name changes

--- Kubenet/Dual Stack/main.tf
+++ Kubenet/Dual Stack/main.tf
@@ -2,35 +2,30 @@ resource "azurerm_resource_group" "kubenetds" {
   name     = "kubenetds"
   location = "canadacentral"
 }
-
 resource "azurerm_virtual_network" "kubenetipv4" {
   name                = "kubenetipv4-vnet"
   address_space       = ["192.168.10.0/24"]
   location            = azurerm_resource_group.kubenetds.location
   resource_group_name = azurerm_resource_group.kubenetds.name
 }
-
 resource "azurerm_subnet" "kubenetipv4subnet" {
   name                 = "kubenetipv4-subnet"
   virtual_network_name = azurerm_virtual_network.kubenetipv4.name
   address_prefixes     = ["192.168.10.0/24"]
   resource_group_name  = azurerm_resource_group.kubenetds.name
 }
-
 resource "azurerm_virtual_network" "kubenetipv6" {
   name                = "kubenetipv6-vnet"
   address_space       = ["1900::/64"]
   location            = azurerm_resource_group.kubenetds.location
   resource_group_name = azurerm_resource_group.kubenetds.name
 }
-
 resource "azurerm_subnet" "kubenetipv6subnet" {
   name                 = "kubenetipv6-subnet"
   virtual_network_name = azurerm_virtual_network.kubenetipv6.name
   address_prefixes     = ["1900::/64"]
   resource_group_name  = azurerm_resource_group.kubenetds.name
 }
-
 resource "azurerm_kubernetes_cluster" "kubenetds" {
   name                = "kubenetdsaks"
   location            = azurerm_resource_group.kubenetds.location
@@ -41,6 +36,7 @@ resource "azurerm_kubernetes_cluster" "kubenetds" {
     node_count = 2
     vm_size    = "Standard_DS2_v2"
   }
+  
   identity {
     type = "SystemAssigned"
   }
