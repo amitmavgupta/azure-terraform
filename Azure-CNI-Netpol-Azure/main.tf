b1d3a188deabe72854e1fd6225681e31b6209da0
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2023-12-28 23:44:10 +0530

    Minor Fixes

--- Azure-CNI-Netpol-Azure/main.tf
+++ Azure-CNI-Netpol-Azure/main.tf
@@ -2,21 +2,18 @@ resource "azurerm_resource_group" "azurecni" {
   name     = "azurecnirg"
   location = "canadacentral"
 }
-
 resource "azurerm_virtual_network" "azurecni" {
   name                = "azurecni-vnet"
   address_space       = ["10.0.0.0/8"]
   location            = azurerm_resource_group.azurecni.location
   resource_group_name = azurerm_resource_group.azurecni.name
 }
-
 resource "azurerm_subnet" "azurecni" {
   name                 = "azurecni-subnet"
   resource_group_name  = azurerm_resource_group.azurecni.name
   virtual_network_name = azurerm_virtual_network.azurecni.name
   address_prefixes     = ["10.10.0.0/16"]
 }
-
 resource "azurerm_kubernetes_cluster" "azurecni" {
   name                = "azurecniaks"
   location            = azurerm_resource_group.azurecni.location
