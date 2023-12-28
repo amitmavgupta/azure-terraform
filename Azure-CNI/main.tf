337efce969e1a0bb854cd97badf40e1928ac4624
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2023-12-28 19:16:24 +0530

    Modify Azure CNI constructs

--- Azure-CNI/main.tf
+++ Azure-CNI/main.tf
@@ -1,31 +1,31 @@
 provider "azurerm" {
   features {}
 }
-resource "azurerm_resource_group" "test" {
-  name     = "acctestRG"
+resource "azurerm_resource_group" "azurecni" {
+  name     = "azurecnirg"
   location = "canadacentral"
 }
 
-resource "azurerm_virtual_network" "test" {
-  name                = "acctestRG-vnet"
+resource "azurerm_virtual_network" "azurecni" {
+  name                = "azurecnirg-vnet"
   address_space       = ["10.0.0.0/8"]
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
 }
 
-resource "azurerm_subnet" "test" {
-  name                 = "acctestRG-subnet"
+resource "azurerm_subnet" "azurecni" {
+  name                 = "azurecnirg-subnet"
   resource_group_name  = azurerm_resource_group.test.name
   virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes     = ["10.10.0.0/16"]
 
 }
 
-resource "azurerm_kubernetes_cluster" "test" {
-  name                = "acctestaks"
+resource "azurerm_kubernetes_cluster" "azurecni" {
+  name                = "azurecniaks"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  dns_prefix          = "acctestaks"
+  dns_prefix          = "azurecniaks"
   default_node_pool {
     name           = "default"
     node_count     = 2
