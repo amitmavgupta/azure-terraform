52be596cd16c775cccced6c72c97dcb0188549ba
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2023-12-28 19:28:56 +0530

    Modify Azure CNI constructs

--- Azure-CNI/main.tf
+++ Azure-CNI/main.tf
@@ -15,7 +15,6 @@ resource "azurerm_subnet" "azurecni" {
   resource_group_name  = azurerm_resource_group.azurecni.name
   virtual_network_name = azurerm_virtual_network.azurecni.name
   address_prefixes     = ["10.10.0.0/16"]
-
 }
 
 resource "azurerm_kubernetes_cluster" "azurecni" {
