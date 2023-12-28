fca47c6a575bb219993f43f0b05437d0dc48e9b8
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2023-12-28 23:42:29 +0530

    Minor Fixes

--- Kubenet/IPv4/main.tf
+++ Kubenet/IPv4/main.tf
@@ -2,14 +2,12 @@ resource "azurerm_resource_group" "kubenetipv4" {
   name     = "kubenetipv4"
   location = "canadacentral"
 }
-
 resource "azurerm_virtual_network" "kubenetipv4" {
   name                = "kubenetipv4-vnet"
   address_space       = ["192.168.10.0/24"]
   location            = azurerm_resource_group.kubenetipv4.location
   resource_group_name = azurerm_resource_group.kubenetipv4.name
 }
-
 resource "azurerm_subnet" "kubenetipv4" {
   name                 = "kubenetipv4-subnet"
   resource_group_name  = azurerm_resource_group.kubenetipv4.name
@@ -17,7 +15,6 @@ resource "azurerm_subnet" "kubenetipv4" {
   address_prefixes     = ["192.168.10.0/24"]
 
 }
-
 resource "azurerm_kubernetes_cluster" "kubenetipv4" {
   name                = "kubenetipv4aks"
   location            = azurerm_resource_group.kubenetipv4.location
@@ -29,9 +26,11 @@ resource "azurerm_kubernetes_cluster" "kubenetipv4" {
     vm_size           = "Standard_DS2_v2"
     vnet_subnet_id    = azurerm_subnet.kubenetipv4.id
   }
+
   identity {
     type = "SystemAssigned"
   }
+  
   network_profile {
     network_plugin   = "kubenet"
     ip_versions      = ["IPv4"]
