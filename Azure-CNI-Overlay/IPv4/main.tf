ade32f1b73a41be54971685e1f827e5ccabe13bd
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2023-12-28 23:46:47 +0530

    Minor Fixes

---
+++ Azure-CNI-Overlay/IPv4/main.tf
@@ -0,0 +1,38 @@
+resource "azurerm_resource_group" "azurecilium" {
+  name     = "azurecilium"
+  location = "canadacentral"
+}
+resource "azurerm_virtual_network" "azurecilium" {
+  name                = "azurecilium-vnet"
+  address_space       = ["192.168.10.0/24"]
+  location            = azurerm_resource_group.azurecilium.location
+  resource_group_name = azurerm_resource_group.azurecilium.name
+}
+resource "azurerm_subnet" "azurecilium" {
+  name                 = "azurecilium-subnet"
+  resource_group_name  = azurerm_resource_group.azurecilium.name
+  virtual_network_name = azurerm_virtual_network.azurecilium.name
+  address_prefixes     = ["192.168.10.0/24"]
+
+}
+resource "azurerm_kubernetes_cluster" "azurecilium" {
+  name                = "azurecilium"
+  location            = azurerm_resource_group.azurecilium.location
+  resource_group_name = azurerm_resource_group.azurecilium.name
+  dns_prefix          = "azurecilium"
+  default_node_pool {
+    name              = "azurecilium"
+    node_count        = 2
+    vm_size           = "Standard_DS2_v2"
+    vnet_subnet_id    = azurerm_subnet.azurecilium.id
+  }
+  
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin	    = "azure"
+    network_plugin_mode	= "overlay"
+  }
+}
