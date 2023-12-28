f77ea4c4f81348640f98ba1a77a14f6d53a1c872
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2023-12-28 23:51:43 +0530

    Minor Fixes

--- Azure-CNI-Overlay/IPv4/main.tf
+++ Azure-CNI-Overlay/IPv4/main.tf
@@ -1,30 +1,30 @@
-resource "azurerm_resource_group" "azurecilium" {
-  name     = "azurecilium"
+resource "azurerm_resource_group" "azcnioverlay" {
+  name     = "azcnioverlay"
   location = "canadacentral"
 }
-resource "azurerm_virtual_network" "azurecilium" {
-  name                = "azurecilium-vnet"
+resource "azurerm_virtual_network" "azcnioverlay" {
+  name                = "azcnioverlay-vnet"
   address_space       = ["192.168.10.0/24"]
-  location            = azurerm_resource_group.azurecilium.location
-  resource_group_name = azurerm_resource_group.azurecilium.name
+  location            = azurerm_resource_group.azcnioverlay.location
+  resource_group_name = azurerm_resource_group.azcnioverlay.name
 }
-resource "azurerm_subnet" "azurecilium" {
-  name                 = "azurecilium-subnet"
-  resource_group_name  = azurerm_resource_group.azurecilium.name
-  virtual_network_name = azurerm_virtual_network.azurecilium.name
+resource "azurerm_subnet" "azcnioverlay" {
+  name                 = "azcnioverlay-subnet"
+  resource_group_name  = azurerm_resource_group.azcnioverlay.name
+  virtual_network_name = azurerm_virtual_network.azcnioverlay.name
   address_prefixes     = ["192.168.10.0/24"]
 
 }
-resource "azurerm_kubernetes_cluster" "azurecilium" {
-  name                = "azurecilium"
-  location            = azurerm_resource_group.azurecilium.location
-  resource_group_name = azurerm_resource_group.azurecilium.name
-  dns_prefix          = "azurecilium"
+resource "azurerm_kubernetes_cluster" "azcnioverlay" {
+  name                = "azcnioverlayaks"
+  location            = azurerm_resource_group.azcnioverlay.location
+  resource_group_name = azurerm_resource_group.azcnioverlay.name
+  dns_prefix          = "azcnioverlayaks"
   default_node_pool {
-    name              = "azurecilium"
+    name              = "azcnioverlay"
     node_count        = 2
     vm_size           = "Standard_DS2_v2"
-    vnet_subnet_id    = azurerm_subnet.azurecilium.id
+    vnet_subnet_id    = azurerm_subnet.azcnioverlay.id
   }
   
   identity {
@@ -34,5 +34,6 @@ resource "azurerm_kubernetes_cluster" "azurecilium" {
   network_profile {
     network_plugin	    = "azure"
     network_plugin_mode	= "overlay"
+    pod_cidr            = "10.10.0.0/16"
   }
 }
