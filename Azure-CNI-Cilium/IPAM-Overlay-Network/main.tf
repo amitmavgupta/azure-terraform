459424eafe1aa835b97728ec7e8661faafcd6a90
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2023-12-29 14:35:28 +0530

    Minor Fix

--- Azure-CNI-Cilium/IPAM-Overlay-Network/main.tf
+++ Azure-CNI-Cilium/IPAM-Overlay-Network/main.tf
@@ -1,37 +1,36 @@
-resource "azurerm_resource_group" "azurecilium" {
-  name     = "azurecilium"
+resource "azurerm_resource_group" "azpcoverlay" {
+  name     = "azpcoverlay"
   location = "canadacentral"
 }
-
-resource "azurerm_virtual_network" "azurecilium" {
-  name                = "azurecilium-vnet"
+resource "azurerm_virtual_network" "azpcoverlay" {
+  name                = "azpcoverlay-vnet"
   address_space       = ["192.168.10.0/24"]
-  location            = azurerm_resource_group.azurecilium.location
-  resource_group_name = azurerm_resource_group.azurecilium.name
+  location            = azurerm_resource_group.azpcoverlay.location
+  resource_group_name = azurerm_resource_group.azpcoverlay.name
 }
-
-resource "azurerm_subnet" "azurecilium" {
-  name                 = "azurecilium-subnet"
-  resource_group_name  = azurerm_resource_group.azurecilium.name
-  virtual_network_name = azurerm_virtual_network.azurecilium.name
+resource "azurerm_subnet" "azpcoverlay" {
+  name                 = "azpcoverlay-subnet"
+  resource_group_name  = azurerm_resource_group.azpcoverlay.name
+  virtual_network_name = azurerm_virtual_network.azpcoverlay.name
   address_prefixes     = ["192.168.10.0/24"]
 
 }
-
-resource "azurerm_kubernetes_cluster" "azurecilium" {
-  name                = "azurecilium"
-  location            = azurerm_resource_group.azurecilium.location
-  resource_group_name = azurerm_resource_group.azurecilium.name
-  dns_prefix          = "azurecilium"
+resource "azurerm_kubernetes_cluster" "azpcoverlay" {
+  name                = "azpcoverlay"
+  location            = azurerm_resource_group.azpcoverlay.location
+  resource_group_name = azurerm_resource_group.azpcoverlay.name
+  dns_prefix          = "azpcoverlay"
   default_node_pool {
-    name           = "azurecilium"
+    name           = "azpcoverlay"
     node_count     = 2
     vm_size        = "Standard_DS2_v2"
-    vnet_subnet_id = azurerm_subnet.azurecilium.id
+    vnet_subnet_id = azurerm_subnet.azpcoverlay.id
   }
+
   identity {
     type = "SystemAssigned"
   }
+  
   network_profile {
     pod_cidr            = "10.10.0.0/22"
     service_cidr        = "10.20.0.0/24"
