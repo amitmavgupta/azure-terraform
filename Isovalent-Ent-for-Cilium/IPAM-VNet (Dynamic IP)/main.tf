0ca7425989891ef910883d8d48980a25504f437d
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2024-01-03 13:48:45 +0530

    Add support for IE4C in Dynamic IP Mode

---
+++ Isovalent-Ent-for-Cilium/IPAM-VNet (Dynamic IP)/main.tf
@@ -0,0 +1,55 @@
+resource "azurerm_resource_group" "azpcdynamic" {
+  name     = "azpcdynamic"
+  location = "canadacentral"
+}
+resource "azurerm_virtual_network" "azpcdynamic" {
+  name                = "azpcdynamic-vnet"
+  address_space       = ["10.0.0.0/8"]
+  location            = azurerm_resource_group.azpcdynamic.location
+  resource_group_name = azurerm_resource_group.azpcdynamic.name
+}
+resource "azurerm_subnet" "azpcdynamicnodes" {
+  name                 = "azpcdynamic-subnet-node"
+  resource_group_name  = azurerm_resource_group.azpcdynamic.name
+  virtual_network_name = azurerm_virtual_network.azpcdynamic.name
+  address_prefixes     = ["10.240.0.0/16"]
+}
+resource "azurerm_subnet" "azpcdynamicpods" {
+  name                 = "azpcdynamic-subnet-pods"
+  resource_group_name  = azurerm_resource_group.azpcdynamic.name
+  virtual_network_name = azurerm_virtual_network.azpcdynamic.name
+  address_prefixes     = ["10.241.0.0/16"]
+}
+resource "azurerm_kubernetes_cluster" "azpcdynamic" {
+  name                = "azpcdynamic"
+  location            = azurerm_resource_group.azpcdynamic.location
+  resource_group_name = azurerm_resource_group.azpcdynamic.name
+  dns_prefix          = "azpcdynamic"
+  default_node_pool {
+    name           = "azpcdynamic"
+    node_count     = 2
+    vm_size        = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.azpcdynamicnodes.id
+    pod_subnet_id  = azurerm_subnet.azpcdynamicpods.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin  = "azure"
+    ebpf_data_plane = "cilium"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_extension" "cilium" {
+  name           = "cilium"
+  cluster_id     = azurerm_kubernetes_cluster.azpcdynamic.id
+  extension_type = "Isovalent.CiliumEnterprise.One"
+  plan {
+    name      = var.plan_name
+    product   = var.plan_product
+    publisher = var.plan_publisher
+  }
+}
