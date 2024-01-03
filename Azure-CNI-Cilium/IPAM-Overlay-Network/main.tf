8bb3183fcef30f7ece305041e9b4ba816f7e9fa1
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2024-01-03 12:24:14 +0530

    Minor Fixes

--- Azure-CNI-Cilium/IPAM-Overlay-Network/main.tf
+++ Azure-CNI-Cilium/IPAM-Overlay-Network/main.tf
@@ -2,35 +2,21 @@ resource "azurerm_resource_group" "azpcoverlay" {
   name     = "azpcoverlay"
   location = "canadacentral"
 }
-resource "azurerm_virtual_network" "azpcoverlay" {
-  name                = "azpcoverlay-vnet"
-  address_space       = ["192.168.10.0/24"]
-  location            = azurerm_resource_group.azpcoverlay.location
-  resource_group_name = azurerm_resource_group.azpcoverlay.name
-}
-resource "azurerm_subnet" "azpcoverlay" {
-  name                 = "azpcoverlay-subnet"
-  resource_group_name  = azurerm_resource_group.azpcoverlay.name
-  virtual_network_name = azurerm_virtual_network.azpcoverlay.name
-  address_prefixes     = ["192.168.10.0/24"]
-
-}
 resource "azurerm_kubernetes_cluster" "azpcoverlay" {
   name                = "azpcoverlay"
   location            = azurerm_resource_group.azpcoverlay.location
   resource_group_name = azurerm_resource_group.azpcoverlay.name
   dns_prefix          = "azpcoverlay"
   default_node_pool {
-    name           = "azpcoverlay"
-    node_count     = 2
-    vm_size        = "Standard_DS2_v2"
-    vnet_subnet_id = azurerm_subnet.azpcoverlay.id
+    name       = "azpcoverlay"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
   }
 
   identity {
     type = "SystemAssigned"
   }
-  
+
   network_profile {
     pod_cidr            = "10.10.0.0/22"
     service_cidr        = "10.20.0.0/24"
