2fd01085816001eae3941ca0228b76ee0851c3ae
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2024-01-03 13:48:26 +0530

    Add support for IE4C in Overlay Mode

---
+++ Isovalent-Ent-for-Cilium/IPAM-Overlay Network/variables.tf
@@ -0,0 +1,16 @@
+# Plan Block Variables
+variable "plan_name" {
+  description = "Plan Name"
+  type        = string
+  #  default  = "isovalent-cilium-enterprise-base-edition"
+}
+variable "plan_product" {
+  description = "Plan Product"
+  type        = string
+  #default    = "isovalent-cilium-enterprise"
+}
+variable "plan_publisher" {
+  description = "Plan Publisher"
+  type        = string
+  #default    = "isovalentinc#############"
+}
