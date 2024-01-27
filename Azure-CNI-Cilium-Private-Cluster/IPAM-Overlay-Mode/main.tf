cc5d4d6a74f8665d7b6a40d063174decd882fb6a
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2024-01-27 10:50:50 +0530

    Support for Private clusters on AZPC Overlay

---
+++ Azure-CNI-Cilium-Private-Cluster/IPAM-Overlay-Mode/main.tf
@@ -0,0 +1,33 @@
+resource "azurerm_resource_group" "azpcoverlaypr" {
+  name     = "azpcoverlaypr"
+  location = "canadacentral"
+}
+resource "azurerm_kubernetes_cluster" "azpcoverlaypr" {
+  name                                = "azpcoverlaypr"
+  location                            = azurerm_resource_group.azpcoverlaypr.location
+  resource_group_name                 = azurerm_resource_group.azpcoverlaypr.name
+  dns_prefix                          = "azpcoverlaypr"
+  private_cluster_enabled             = true
+  private_cluster_public_fqdn_enabled = true
+  private_dns_zone_id                 = "None"
+  default_node_pool {
+    name           = "azpcoverlaypr"
+    node_count     = 2
+    vm_size        = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    pod_cidr            = "10.10.0.0/22"
+    service_cidr        = "10.20.0.0/24"
+    dns_service_ip      = "10.20.0.10"
+    network_policy      = "cilium"
+    load_balancer_sku   = "standard"
+    network_plugin      = "azure"
+    network_plugin_mode = "overlay"
+    ebpf_data_plane     = "cilium"
+  }
+}
