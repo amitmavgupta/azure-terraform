af81cc991f8a1a7e068c2cea546f0706ba9e5beb
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2024-04-16 21:10:03 +0530

    Add Support for Dual Stack in BYOCNI

--- BYOCNI/Dual Stack/main.tf
+++ BYOCNI/Dual Stack/main.tf
@@ -18,6 +18,7 @@ resource "azurerm_kubernetes_cluster" "byocnids" {
   name                = "byocnidsaks"
   location            = azurerm_resource_group.byocnids.location
   resource_group_name = azurerm_resource_group.byocnids.name
+  kubernetes_version  = 1.29
   dns_prefix          = "byocnidsaks"
   default_node_pool {
     name           = "byocnids"
