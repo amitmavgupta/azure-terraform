81c9569630275973d70001bbed40c0c308ec29f8
Amit Gupta <115551423+amitmavgupta@users.noreply.github.com>
2024-04-17 09:43:30 +0530

    Update main.tf
    Signed-off-by: Amit Gupta <115551423+amitmavgupta@users.noreply.github.com>

--- BYOCNI-Azure-Linux/Dual Stack/main.tf
+++ BYOCNI-Azure-Linux/Dual Stack/main.tf
@@ -1,31 +1,31 @@
-resource "azurerm_resource_group" "byocnids" {
-  name     = "byocnids"
+resource "azurerm_resource_group" "byocnidsal" {
+  name     = "byocnidsal"
   location = "canadacentral"
 }
-resource "azurerm_virtual_network" "byocnids" {
-  name                = "byocnids-vnet"
+resource "azurerm_virtual_network" "byocnidsal" {
+  name                = "byocnidsal-vnet"
   address_space       = ["192.168.10.0/24", "1900::/64"]
-  location            = azurerm_resource_group.byocnids.location
-  resource_group_name = azurerm_resource_group.byocnids.name
+  location            = azurerm_resource_group.byocnidsal.location
+  resource_group_name = azurerm_resource_group.byocnidsal.name
 }
-resource "azurerm_subnet" "byocnids" {
-  name                 = "byocnids-subnet"
-  virtual_network_name = azurerm_virtual_network.byocnids.name
+resource "azurerm_subnet" "byocnidsal" {
+  name                 = "byocnidsal-subnet"
+  virtual_network_name = azurerm_virtual_network.byocnidsal.name
   address_prefixes     = ["192.168.10.0/24", "1900::/64"]
-  resource_group_name  = azurerm_resource_group.byocnids.name
+  resource_group_name  = azurerm_resource_group.byocnidsal.name
 }
-resource "azurerm_kubernetes_cluster" "byocnids" {
-  name                = "byocnidsaks"
-  location            = azurerm_resource_group.byocnids.location
-  resource_group_name = azurerm_resource_group.byocnids.name
+resource "azurerm_kubernetes_cluster" "byocnidsal" {
+  name                = "byocnidsalaks"
+  location            = azurerm_resource_group.byocnidsal.location
+  resource_group_name = azurerm_resource_group.byocnidsal.name
   kubernetes_version  = 1.29
-  dns_prefix          = "byocnidsaks"
+  dns_prefix          = "byocnidsalaks"
   default_node_pool {
-    name           = "byocnids"
+    name           = "byocnidsal"
     node_count     = 2
     vm_size        = "Standard_DS2_v2"
     os_sku         = "AzureLinux"
-    vnet_subnet_id = azurerm_subnet.byocnids.id
+    vnet_subnet_id = azurerm_subnet.byocnidsal.id
   }
 
   identity {
