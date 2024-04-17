a218e074c00b47f31a7e613d0bee939aea77ea5b
Amit Gupta <115551423+amitmavgupta@users.noreply.github.com>
2024-04-17 09:42:57 +0530

    Update main.tf
    Signed-off-by: Amit Gupta <115551423+amitmavgupta@users.noreply.github.com>

--- BYOCNI-Azure-Linux/Dual Stack/main.tf
+++ BYOCNI-Azure-Linux/Dual Stack/main.tf
@@ -1,31 +1,31 @@
-resource "azurerm_resource_group" "byocnidsal" {
-  name     = "byocnidsal"
+resource "azurerm_resource_group" "byocnids" {
+  name     = "byocnids"
   location = "canadacentral"
 }
-resource "azurerm_virtual_network" "byocnidsal" {
-  name                = "byocnidsal-vnet"
-  address_space       = ["192.168.10.0/24"]
-  location            = azurerm_resource_group.byocnidsal.location
-  resource_group_name = azurerm_resource_group.byocnidsal.name
+resource "azurerm_virtual_network" "byocnids" {
+  name                = "byocnids-vnet"
+  address_space       = ["192.168.10.0/24", "1900::/64"]
+  location            = azurerm_resource_group.byocnids.location
+  resource_group_name = azurerm_resource_group.byocnids.name
 }
-resource "azurerm_subnet" "byocnidsal" {
-  name                 = "byocnidsal-subnet"
-  resource_group_name  = azurerm_resource_group.byocnidsal.name
-  virtual_network_name = azurerm_virtual_network.byocnidsal.name
-  address_prefixes     = ["192.168.10.0/24"]
+resource "azurerm_subnet" "byocnids" {
+  name                 = "byocnids-subnet"
+  virtual_network_name = azurerm_virtual_network.byocnids.name
+  address_prefixes     = ["192.168.10.0/24", "1900::/64"]
+  resource_group_name  = azurerm_resource_group.byocnids.name
 }
-resource "azurerm_kubernetes_cluster" "byocnidsal" {
-  name                = "byocnidsalaks"
-  location            = azurerm_resource_group.byocnidsal.location
-  resource_group_name = azurerm_resource_group.byocnidsal.name
+resource "azurerm_kubernetes_cluster" "byocnids" {
+  name                = "byocnidsaks"
+  location            = azurerm_resource_group.byocnids.location
+  resource_group_name = azurerm_resource_group.byocnids.name
   kubernetes_version  = 1.29
-  dns_prefix          = "byocnidsalaks"
+  dns_prefix          = "byocnidsaks"
   default_node_pool {
-    name           = "byocnidsal"
+    name           = "byocnids"
     node_count     = 2
     vm_size        = "Standard_DS2_v2"
     os_sku         = "AzureLinux"
-    vnet_subnet_id = azurerm_subnet.byocnidsal.id
+    vnet_subnet_id = azurerm_subnet.byocnids.id
   }
 
   identity {
@@ -33,9 +33,10 @@ resource "azurerm_kubernetes_cluster" "byocnidsal" {
   }
 
   network_profile {
-    pod_cidr       = "10.10.0.0/22"
-    service_cidr   = "10.20.0.0/24"
-    dns_service_ip = "10.20.0.10"
     network_plugin = "none"
+    ip_versions    = ["IPv4", "IPv6"]
+    pod_cidrs      = ["10.10.0.0/22", "2001::/64"]
+    service_cidrs  = ["10.20.0.0/24", "2001:1::/108"]
+    dns_service_ip = "10.20.0.10"
   }
 }
