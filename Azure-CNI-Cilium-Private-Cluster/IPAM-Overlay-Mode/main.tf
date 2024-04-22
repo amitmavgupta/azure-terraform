752a02f44e6fe87c143a3e8228dc9e11112a254b
Amit Gupta <115551423+amitmavgupta@users.noreply.github.com>
2024-04-22 10:09:03 +0530

    Update main.tf
    Signed-off-by: Amit Gupta <115551423+amitmavgupta@users.noreply.github.com>

--- Azure-CNI-Cilium-Private-Cluster/IPAM-Overlay-Mode/main.tf
+++ Azure-CNI-Cilium-Private-Cluster/IPAM-Overlay-Mode/main.tf
@@ -1,18 +1,18 @@
-resource "azurerm_resource_group" "azpcoverlaypr" {
-  name     = "azpcoverlaypr"
+resource "azurerm_resource_group" "azpcolaypr" {
+  name     = "azpcolaypr"
   location = "canadacentral"
 }
-resource "azurerm_kubernetes_cluster" "azpcoverlaypr" {
-  name                                = "azpcoverlaypr"
-  location                            = azurerm_resource_group.azpcoverlaypr.location
-  resource_group_name                 = azurerm_resource_group.azpcoverlaypr.name
+resource "azurerm_kubernetes_cluster" "azpcolaypr" {
+  name                                = "azpcolaypr"
+  location                            = azurerm_resource_group.azpcolaypr.location
+  resource_group_name                 = azurerm_resource_group.azpcolaypr.name
   kubernetes_version  = 1.29
-  dns_prefix                          = "azpcoverlaypr"
+  dns_prefix                          = "azpcolaypr"
   private_cluster_enabled             = true
   private_cluster_public_fqdn_enabled = true
   private_dns_zone_id                 = "System"
   default_node_pool {
-    name           = "azpcoverlaypr"
+    name           = "azpcolaypr"
     node_count     = 2
     vm_size        = "Standard_DS2_v2"
   }
