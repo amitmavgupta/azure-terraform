06baa7ebf0a6d00e949c86068933bda337efa19d
Amit Gupta <115551423+amitmavgupta@users.noreply.github.com>
2024-04-22 09:56:12 +0530

    Update main.tf
    Signed-off-by: Amit Gupta <115551423+amitmavgupta@users.noreply.github.com>

--- BYOCNI-Private-Cluster/main.tf
+++ BYOCNI-Private-Cluster/main.tf
@@ -1,33 +1,33 @@
-resource "azurerm_resource_group" "byopraks" {
-  name     = "byopraks"
+resource "azurerm_resource_group" "byopr" {
+  name     = "byopr"
   location = "canadacentral"
 }
-resource "azurerm_virtual_network" "byopraks" {
-  name                = "byopraks-vnet"
+resource "azurerm_virtual_network" "byopr" {
+  name                = "byopr-vnet"
   address_space       = ["192.168.10.0/24"]
-  location            = azurerm_resource_group.byopraks.location
-  resource_group_name = azurerm_resource_group.byopraks.name
+  location            = azurerm_resource_group.byopr.location
+  resource_group_name = azurerm_resource_group.byopr.name
 }
-resource "azurerm_subnet" "byopraks" {
-  name                 = "byopraks-subnet"
-  resource_group_name  = azurerm_resource_group.byopraks.name
-  virtual_network_name = azurerm_virtual_network.byopraks.name
+resource "azurerm_subnet" "byopr" {
+  name                 = "byopr-subnet"
+  resource_group_name  = azurerm_resource_group.byopr.name
+  virtual_network_name = azurerm_virtual_network.byopr.name
   address_prefixes     = ["192.168.10.0/24"]
 }
-resource "azurerm_kubernetes_cluster" "byopraks" {
-  name                = "byopraks"
-  location            = azurerm_resource_group.byopraks.location
-  resource_group_name = azurerm_resource_group.byopraks.name
+resource "azurerm_kubernetes_cluster" "byopr" {
+  name                = "byopr"
+  location            = azurerm_resource_group.byopr.location
+  resource_group_name = azurerm_resource_group.byopr.name
   kubernetes_version  = 1.29
-  dns_prefix          = "byopraks"
+  dns_prefix          = "byopr"
   private_cluster_enabled  = true
   private_cluster_public_fqdn_enabled = true
   private_dns_zone_id = "System"
   default_node_pool {
-    name           = "byopraks"
+    name           = "byopr"
     node_count     = 2
     vm_size        = "Standard_DS2_v2"
-    vnet_subnet_id = azurerm_subnet.byopraks.id
+    vnet_subnet_id = azurerm_subnet.byopr.id
   }
   identity {
     type = "SystemAssigned"
