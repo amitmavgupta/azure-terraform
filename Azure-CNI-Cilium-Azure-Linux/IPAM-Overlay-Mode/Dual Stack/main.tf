96c647d46a4e5187b1d86556125a80392f164c48
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2024-04-22 10:49:08 +0530

    add support for Dual Stack in AZPC Overlay with Azure Linux as host OS

---
+++ Azure-CNI-Cilium-Azure-Linux/IPAM-Overlay-Mode/Dual Stack/main.tf
@@ -0,0 +1,31 @@
+resource "azurerm_resource_group" "azpcoverlay" {
+  name     = "azpcoverlay"
+  location = "canadacentral"
+}
+resource "azurerm_kubernetes_cluster" "azpcoverlay" {
+  name                = "azpcoverlay"
+  location            = azurerm_resource_group.azpcoverlay.location
+  resource_group_name = azurerm_resource_group.azpcoverlay.name
+  kubernetes_version  = 1.29
+  dns_prefix          = "azpcoverlay"
+  default_node_pool {
+    name       = "azpcoverlay"
+    node_count = 2
+    os_sku     = "AzureLinux"
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin      = "azure"
+    network_plugin_mode = "overlay"
+    ebpf_data_plane     = "cilium"
+    ip_versions         = ["IPv4", "IPv6"]
+    pod_cidrs           = ["10.10.0.0/22", "2001::/64"]
+    service_cidrs       = ["10.20.0.0/24", "2001:1::/108"]
+    dns_service_ip      = "10.20.0.10"
+  }
+}
