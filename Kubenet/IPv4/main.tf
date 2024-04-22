2550bd2df695157a703f0e617232bba741dbc32b
Amit Gupta <115551423+amitmavgupta@users.noreply.github.com>
2024-04-22 09:53:16 +0530

    Update main.tf
    Signed-off-by: Amit Gupta <115551423+amitmavgupta@users.noreply.github.com>

--- Kubenet/IPv4/main.tf
+++ Kubenet/IPv4/main.tf
@@ -1,30 +1,30 @@
-resource "azurerm_resource_group" "kubenetipv4" {
-  name     = "kubenetipv4"
+resource "azurerm_resource_group" "kubenet" {
+  name     = "kubenet"
   location = "canadacentral"
 }
-resource "azurerm_virtual_network" "kubenetipv4" {
-  name                = "kubenetipv4-vnet"
+resource "azurerm_virtual_network" "kubenet" {
+  name                = "kubenet-vnet"
   address_space       = ["192.168.10.0/24"]
-  location            = azurerm_resource_group.kubenetipv4.location
-  resource_group_name = azurerm_resource_group.kubenetipv4.name
+  location            = azurerm_resource_group.kubenet.location
+  resource_group_name = azurerm_resource_group.kubenet.name
 }
-resource "azurerm_subnet" "kubenetipv4" {
-  name                 = "kubenetipv4-subnet"
-  resource_group_name  = azurerm_resource_group.kubenetipv4.name
-  virtual_network_name = azurerm_virtual_network.kubenetipv4.name
+resource "azurerm_subnet" "kubenet" {
+  name                 = "kubenet-subnet"
+  resource_group_name  = azurerm_resource_group.kubenet.name
+  virtual_network_name = azurerm_virtual_network.kubenet.name
   address_prefixes     = ["192.168.10.0/24"]
 }
-resource "azurerm_kubernetes_cluster" "kubenetipv4" {
-  name                = "kubenetipv4aks"
-  location            = azurerm_resource_group.kubenetipv4.location
-  resource_group_name = azurerm_resource_group.kubenetipv4.name
+resource "azurerm_kubernetes_cluster" "kubenet" {
+  name                = "kubenetaks"
+  location            = azurerm_resource_group.kubenet.location
+  resource_group_name = azurerm_resource_group.kubenet.name
   kubernetes_version  = 1.29
-  dns_prefix          = "kubenetipv4aks"
+  dns_prefix          = "kubenetaks"
   default_node_pool {
-    name              = "kubenetipv4"
+    name              = "kubenet"
     node_count        = 2
     vm_size           = "Standard_DS2_v2"
-    vnet_subnet_id    = azurerm_subnet.kubenetipv4.id
+    vnet_subnet_id    = azurerm_subnet.kubenet.id
   }
 
   identity {
