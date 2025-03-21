efde5903fb3002f19a79e34513cce63609c68804
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2025-03-21 14:39:06 +0530

    BYOCNI-Azure-Linux-v3-support-k8s=1.31

---
+++ BYOCNI-Azure-Linux-v3/main.tf
@@ -0,0 +1,41 @@
+resource "azurerm_resource_group" "byocnial" {
+  name     = "byocnial"
+  location = "canadacentral"
+}
+resource "azurerm_virtual_network" "byocnial" {
+  name                = "byocnial-vnet"
+  address_space       = ["192.168.10.0/24"]
+  location            = azurerm_resource_group.byocnial.location
+  resource_group_name = azurerm_resource_group.byocnial.name
+}
+resource "azurerm_subnet" "byocnial" {
+  name                 = "byocnial-subnet"
+  resource_group_name  = azurerm_resource_group.byocnial.name
+  virtual_network_name = azurerm_virtual_network.byocnial.name
+  address_prefixes     = ["192.168.10.0/24"]
+}
+resource "azurerm_kubernetes_cluster" "byocnial" {
+  name                = "byocnial"
+  location            = azurerm_resource_group.byocnial.location
+  resource_group_name = azurerm_resource_group.byocnial.name
+  kubernetes_version  = 1.31
+  dns_prefix          = "byocnial"
+  default_node_pool {
+    name           = "byocnial"
+    node_count     = 2
+    vm_size        = "Standard_DS2_v2"
+    os_sku         = "AzureLinux"
+    vnet_subnet_id = azurerm_subnet.byocnial.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    pod_cidr       = "10.10.0.0/22"
+    service_cidr   = "10.20.0.0/24"
+    dns_service_ip = "10.20.0.10"
+    network_plugin = "none"
+  }
+}
