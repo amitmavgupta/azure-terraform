de39f139d69fc10d1d3e4f4e19b8bcf81dd91443
Amit Gupta <115551423+amitmavgupta@users.noreply.github.com>
2025-03-22 21:36:51 +0530

    Update terraform.tf
    Signed-off-by: Amit Gupta <115551423+amitmavgupta@users.noreply.github.com>

--- Isovalent-Ent-for-Cilium-Azure-Linux/IPAM-VNet (Dynamic IP)/terraform.tf
+++ Isovalent-Ent-for-Cilium-Azure-Linux/IPAM-VNet (Dynamic IP)/terraform.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.64.0"
+      version = "~>3.90.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -20,7 +20,6 @@ terraform {
     }
   }
 }
-
 provider "azurerm" {
   features {}
 }
